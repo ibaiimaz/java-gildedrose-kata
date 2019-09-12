b2c78e3c3d2fc0cabda548dac67a5f6a737216eb
ibaiimaz <ibaiimaz@gmail.com>
2019-09-12 16:52:50 +0200

    Separate logic by item type

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -6,7 +6,7 @@ class GildedRose {
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     public static final int ALLOWED_MIN_QUALITY = 0;
     public static final int ALLOWED_MAX_QUALITY = 50;
-    public static final int ITEM_QUALITY_DECREASE_DOUBLE_SELL_IN_LIMIT = 0;
+    public static final int REGULAR_ITEM_QUALITY_DECREASE_DOUBLE_AND_BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_LIMIT = 0;
     public static final int NO_QUALITY = 0;
     Item[] items;
 
@@ -16,55 +16,83 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE)
-                    && !item.name.equals(BACKSTAGE_PASSES)
+
+            if (isRegularItem(item)
             ) {
                 if (item.quality > ALLOWED_MIN_QUALITY) {
-                    if (!item.name.equals(SULFURAS)) {
+                    item.quality = item.quality - 1;
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < REGULAR_ITEM_QUALITY_DECREASE_DOUBLE_AND_BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_LIMIT) {
+                    if (item.quality > ALLOWED_MIN_QUALITY) {
                         item.quality = item.quality - 1;
                     }
                 }
-            } else {
+            }
+
+            if (item.name.equals(AGED_BRIE)) {
                 if (item.quality < ALLOWED_MAX_QUALITY) {
                     item.quality = item.quality + 1;
-
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < ALLOWED_MAX_QUALITY) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < ALLOWED_MAX_QUALITY) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
                 }
-            }
 
-            if (!item.name.equals(SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
-            }
 
-            if (item.sellIn < ITEM_QUALITY_DECREASE_DOUBLE_SELL_IN_LIMIT) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > ALLOWED_MIN_QUALITY) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = NO_QUALITY;
-                    }
-                } else {
+                if (item.sellIn < REGULAR_ITEM_QUALITY_DECREASE_DOUBLE_AND_BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_LIMIT) {
                     if (item.quality < ALLOWED_MAX_QUALITY) {
                         item.quality = item.quality + 1;
                     }
                 }
             }
+
+            if (item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.quality < ALLOWED_MAX_QUALITY) {
+                    item.quality = item.quality + 1;
+
+                    if (item.sellIn < 11) {
+                        if (item.quality < ALLOWED_MAX_QUALITY) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < ALLOWED_MAX_QUALITY) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < REGULAR_ITEM_QUALITY_DECREASE_DOUBLE_AND_BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_LIMIT) {
+                    item.quality = NO_QUALITY;
+                }
+            }
+
+//            if (item.sellIn < REGULAR_ITEM_QUALITY_DECREASE_DOUBLE_AND_BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_LIMIT) {
+//                if (!item.name.equals(AGED_BRIE)) {
+////                    if (!item.name.equals(BACKSTAGE_PASSES)) {
+//////                        if (item.quality > ALLOWED_MIN_QUALITY) {
+//////                            if (!item.name.equals(SULFURAS)) {
+//////                                item.quality = item.quality - 1;
+//////                            }
+//////                        }
+////                    } else {
+////                        item.quality = NO_QUALITY;
+////                    }
+//                } else {
+//                    if (item.quality < ALLOWED_MAX_QUALITY) {
+//                        item.quality = item.quality + 1;
+//                    }
+//                }
+//            }
         }
+    }
+
+    private boolean isRegularItem(Item item) {
+        return !item.name.equals(AGED_BRIE) &&
+            !item.name.equals(BACKSTAGE_PASSES) &&
+            !item.name.equals(SULFURAS);
     }
 }
